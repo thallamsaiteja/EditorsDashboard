04a3df168156d8224a97c3baf1fcb1f5bc7fdc01
ariyan-0p <likuariyan2002@gmail.com>
2025-09-19 11:15:49 +0530

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
-import { Navigate, Route, Routes, BrowserRouter } from 'react-router-dom'
-import ManagerPage from './Component/Manager/ManagerPage'
-import Login from './Component/Auth/Login/Login'
-import Registration from './Component/Auth/Registration/Registration' 
-import './App.css'
+// Import all your page components
+import Homepage from './Component/Home/homepage.jsx';
+import LoginPage from './Component/Auth/Login/Login.jsx';
+import RegistrationPage from './Component/Auth/Registration/Registration.jsx';
+import ForgotPasswordPage from './Component/Auth/ForgotPassword/ForgotPassword.jsx';
 
 function App() {
   return (
-
     <BrowserRouter>
-
       <Routes>
+        {/* Main route: Shows the Homepage */}
+        <Route path='/' element={<Homepage />} />
 
-        <Route path='/managerdashboard' element={<ManagerPage />} />
-        <Route path='/Login' element={<Login />} />
-        <Route path='/Registration' element={<Registration />} />
+        {/* Auth routes for your teammate's pages */}
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegistrationPage />} />
+        <Route path='/forgot-password' element={<ForgotPasswordPage />} />
+        
+        {/* You can add your manager dashboard route here later */}
+        {/* <Route path='/managerdashboard' element={<ManagerPage />} /> */}
       </Routes>
     </BrowserRouter>
-
-  )
+  );
 }
 
-export default App
+export default App;
