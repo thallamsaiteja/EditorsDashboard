ab0ffb47f3d1de4de2563c4290cd5a62c93d9cf2
Sai Teja <saitejathallam@gmail.com>
2025-09-26 16:44:28 +0530

    changes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
   // Specific API endpoints using Vite's import.meta.env
-  AUTH_API: `${import.meta.env.VITE_REACT_APP_API_URL }/api/v1/auth`,
-  MANAGER_API: `${import.meta.env.VITE_REACT_APP_API_URL }/api/v1/manager`,
-  EDITOR_API: `${import.meta.env.VITE_REACT_APP_API_URL }/api/v1/editor`,
+  AUTH_API: `${import.meta.env.VITE_REACT_APP_API_URL}/api/v1/auth`,
+  MANAGER_API: `${import.meta.env.VITE_REACT_APP_API_URL}/api/v1/manager`,
+  EDITOR_API: `${import.meta.env.VITE_REACT_APP_API_URL}/api/v1/editor`,
   ADMIN_API: `${import.meta.env.VITE_REACT_APP_API_URL}/api/v1/admin`,
 
   // Telegram webhook
